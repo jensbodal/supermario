0a954a86d57037572d59975c3021120e8959e06c
jensbodal <jensbodal@jensbodal6850>
2013-12-01 19:46:37 -0800

    Initial Commit

---
+++ src/supermario/Supermario.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package supermario;
+
+/**
+ *
+ * @author jensbodal
+ * @version 1.0
+ * 
+ * Compiler Java 1.7 OS: Windows 7, OSX Hardware: PC
+ * 
+ * Date Day, Year Initials Completed v#
+ */
+public class Supermario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
